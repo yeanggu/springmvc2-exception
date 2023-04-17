48fcd0eee078b0832dfffa187c589211d7d4f1c3
yeeun <dpdms9621@naver.com>
2023-04-17 17:52:44 +0900

    [feat] @ControllerAdvice

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,4 +1,4 @@
-package hello.exception.exhandler;
+package hello.exception.api;
 
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
@@ -11,27 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 public class ApiExceptionV2Controller {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-        log.error("[exceptionHandle] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandle(Exception e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
 
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
